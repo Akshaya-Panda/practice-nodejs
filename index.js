e9ea2cd44bfd34cba3b3ce6c1c84e2c4c3718a5b
AKSHAYA PANDA <89294828+Akshaya-Panda@users.noreply.github.com>
2024-07-15 22:54:53 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+  app.get('/app/api/v1/state.js', function(req, res) {
+    var state = {
+      config: {
+        websocketUrl: (function() {
+          var wsUrl = url.parse(options.websocketUrl, true)
+          wsUrl.query.uip = req.ip
+          return url.format(wsUrl)
+        })()
+      }
+    ,
+     user: req.user
+
+     
+
+    }
+
+    
+
+    if (options.userProfileUrl) {
+      state.config.userProfileUrl = (function() {
+        return options.userProfileUrl
+      })()
+    }
+
+    res.type('application/javascript')
+    res.send('var GLOBAL_APPSTATE = ' + JSON.stringify(state))
+  })
